75762ebc57fd08901dd8c44a2c10362402960a1b
puras <he@puras.cn>
2021-03-12 19:02:57 +0800

    Update code

---
+++ src/components/right-panel/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div ref="rightPanel" :class="{show: show}" class="right-panel-container">
+    <div class="right-panel-background" />
+    <div class="right-panel">
+      <div class="handle-button" :style="{'top': buttonTop + 'px', 'background-color': theme}" @click="show = !show">
+        <i :class="show ? 'el-icon-close': 'el-icon-setting'" />
+      </div>
+      <div class="right-panel-items">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'RightPanel',
+  props: {
+    clickNotClose: {
+      default: false,
+      type: Boolean
+    },
+    buttonTop: {
+      default: 250,
+      type: Number
+    }
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    theme(): string {
+      return 'black'
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/layout/components/sidebar/index.vue
+++ src/layout/components/sidebar/index.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <logo :collapse="collapse" />
+  <div :class="{'has-logo': showLogo}">
+    <logo v-if="showLogo" :collapse="isCollapse" />
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+          :default-active="activeMenu"
+          :collapse="isCollapse"
+          :background-color="variables.menuBg"
+          :text-color="variables.menuText"
+          :unique-opened="false"
+          :active-text-color="variables.menuActiveText"
+          :collapse-transition="false"
+          mode="vertical"
+      ></el-menu>
+    </el-scrollbar>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Logo from './logo.vue'
+import variables from '@/assets/less/vars.less'
 
 export default defineComponent({
   name: 'Sidebar',
@@ -15,7 +28,26 @@ export default defineComponent({
   },
   data() {
     return {
-      collapse: false
+    }
+  },
+  computed: {
+    activeMenu(): string {
+      const route = this.$route
+      const { meta, path } = route
+      if (meta.activeMenu) {
+        return meta.activeMenu as string
+      }
+      return path
+    },
+    showLogo(): boolean {
+      return true
+    },
+    variables(): any {
+      // console.log(variables.menuBg, 'menuBg')
+      return variables
+    },
+    isCollapse(): boolean {
+      return false
     }
   }
 })
